5fa25d9e3603d4f0c7059a86988df6ec9f925151
Franklin borda <fronaldbq@gmail.com>
2025-07-19 08:09:24 -0500

    Delete Pagina web TRAMITE DOCUMENTARIO directory

--- Pagina web TRAMITE DOCUMENTARIO/parte1/PAGINA WEB TRAMITE DOCUMENTARIO Part1  (demo ) SiamSoft/script.js
+++
@@ -1,7 +0,0 @@
-function redirigirPagina() {
-  // Muestra un mensaje
-  alert("Gracias por tu interés en el Software SIAM. Nos pondremos en contacto contigo pronto.");
-
-  //  redirige a la página oficial de SiamSoft
-  window.location.href = "https://www.siamsoft.net";
-}
